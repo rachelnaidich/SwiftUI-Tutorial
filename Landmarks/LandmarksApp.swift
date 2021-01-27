c2f000d5a918ac7bec2e0baff8e64834b3f83ba6
Rachel Naidich <rachelnaidich1@gmail.com>
2021-01-26 21:13:19 -0800

    initialized project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rachel Naidich on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
